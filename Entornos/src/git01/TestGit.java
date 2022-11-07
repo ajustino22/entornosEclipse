f530143454bce1c13bfc325400e8a864f9e69625
Adrián Justino Maestre <ajustino22@ilg.cat>
2022-11-07 14:04:07 +0100

    stage+commit branch-2

--- Entornos/src/git01/TestGit.java
+++ Entornos/src/git01/TestGit.java
@@ -3,7 +3,10 @@ package git01;
 public class TestGit {
 	
 	public static void main(String[] args) {
-		String name = "Bugs Bunny";
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
 		System.out.println("Hello " + name + "!");
+		scanner.close();
+
 	}
 }
