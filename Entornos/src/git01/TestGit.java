d8f18d654374f5a155ce63fdbb9bb310ec37c6da
Adrián Justino Maestre <ajustino22@ilg.cat>
2022-11-07 14:06:41 +0100

    stage+commit branch-1 de nuevo

--- Entornos/src/git01/TestGit.java
+++ Entornos/src/git01/TestGit.java
@@ -1,10 +1,9 @@
 package git01;
-
-public class TestGit {
 	
+public class TestGit {
 	public static void main(String[] args) {
 		String name = "Bugs Bunny";
-		System.out.println("Hello " + name.toUpperCase() + "!");
-
+		System.out.println("Hello Mr. " + name.toUpperCase() + "!");
 	}
 }
+
